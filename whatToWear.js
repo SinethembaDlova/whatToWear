9151b29df653e838304db74ab74d38ea333d3380
SinethembaDlova <dSinethemba@gmail.com>
2017-05-05 16:52:44 +0200

    wardrope updated

--- whatToWear.js
+++ whatToWear.js
@@ -1,40 +1,87 @@
 var inputWeather = document.querySelector("#inputWeather");
 var displayClothes = document.querySelector(".clothesToWearDiv");
 
-var wardrobe = [{
-        image:'Beany.jpg',
-        itemName: 'Beany',
-        key:  'cold'
-    },
+var wardrobe = [
+  {
+      image: 't-shirt.jpg',
+      itemName: 'T-shirt',
+      key: 'sunny'
+  },
 
-    {
-        image: 'cap.jpg',
-        itemName: 'Cap',
-        key: 'sunny'
-    },
-    {
-        image: 'chino.jpg',
-        itemName: 'Chino',
-        key: 'Cool'
-    },
-    {
-        image: 'coat.jpg',
-        itemName: 'Coat',
-        key: 'cold'
-    },
+  {
+      image: 'shortpants.jpg',
+      itemName: 'Shorts',
+      key:  'sunny'
+  },
+  {   image: 'cap.jpg',
+      itemName: 'Cap',
+      key: 'sunny'
+  },
+  {
+      image: 't-shirt2.jpg',
+      itemName: 'T-Shirt',
+      key: 'windy'
+  },
+  {
+      image: 'track-bottom.jpg',
+      itemName: 'Track-bottom',
+      key: 'windy'
+  },
 
-    {
-        image: 'jeanshorts.jpg',
-        itemName: 'Shorts',
-        key:  'sunny'
-    },
+  {
+      image: 'kwaysoftshell.jpg',
+      itemName: 'Sott shell',
+      key: 'windy'
+  },
+  {
+      image:'Beany.jpg',
+      itemName: 'Beannie',
+      key:  'windy'
+  },
 
-    {
-        image: 't-shirt.jpg',
-        itemName: 'T-shirt',
-        key: 'sunny'
-    },
 
+  {
+      image: 'longsleeve.jpg',
+      itemName: 'Long sleeve t-shirt',
+      key: 'cold'
+  },
+  {
+      image: 'chino.jpg',
+      itemName: 'Chino',
+      key: 'Cool'
+  },
+  {
+      image: 'coat.jpg',
+      itemName: 'Coat',
+      key: 'cold'
+  },
+
+  {
+      image: 'beany2.jpg',
+      itemName: 'Beannie',
+      key: 'cold'
+  },
+  {
+      image: 'sweater.jpg',
+      itemName: 'Jersey',
+      key:  'rainy'
+  },
+  {
+      image: 'jeans.jpg',
+      itemName: 'Jean',
+      key: 'rainy'
+  },
+  {
+      image: 'kwayJacket.jpg',
+      itemName: 'Jacket',
+      key:  'rainy'
+  },
+
+  {
+      image: 'winterbennie.jpg',
+      itemName: 'Beannie',
+      key:  'rainy'
+  },
 ];
 
 var wardrobeTemplate = document.querySelector("#wardrobeTemplate").innerHTML;
