0f254d64820b5e597716721d0d54a319842710e1
SinethembaDlova <dSinethemba@gmail.com>
2017-05-08 17:27:31 +0200

    trying to renderfor days

--- whatToWear.js
+++ whatToWear.js
@@ -1,127 +1,125 @@
 var inputWeather = document.querySelector("#inputWeather");
 var displayClothes = document.querySelector(".clothesToWearDiv");
 
-var wardrobe = [
-  {
-      image: 't-shirt.jpg',
-      itemName: 'T-shirt',
-      key: 'sunny'
-  },
+var wardrobe = [{
+        image: 't-shirt.jpg',
+        itemName: 'T-shirt',
+        key: 'sunny'
+    },
 
-  {
-      image: 'shortpants.jpg',
-      itemName: 'Shorts',
-      key:  'sunny'
-  },
-  {   image: 'cap.jpg',
-      itemName: 'Cap',
-      key: 'sunny'
-  },
-  {
-      image: 't-shirt2.jpg',
-      itemName: 'T-Shirt',
-      key: 'windy'
-  },
-  {
-      image: 'track-bottom.jpg',
-      itemName: 'Track-bottom',
-      key: 'windy'
-  },
+    {
+        image: 'shortpants.jpg',
+        itemName: 'Shorts',
+        key: 'sunny'
+    },
+    {
+        image: 'cap.jpg',
+        itemName: 'Cap',
+        key: 'sunny'
+    },
+    {
+        image: 't-shirt2.jpg',
+        itemName: 'T-Shirt',
+        key: 'windy'
+    },
+    {
+        image: 'track-bottom.jpg',
+        itemName: 'Track-bottom',
+        key: 'windy'
+    },
 
-  {
-      image: 'kwaysoftshell.jpg',
-      itemName: 'Sott shell',
-      key: 'windy'
-  },
-  {
-      image:'Beany.jpg',
-      itemName: 'Beannie',
-      key:  'windy'
-  },
+    {
+        image: 'kwaysoftshell.jpg',
+        itemName: 'Sott shell',
+        key: 'windy'
+    },
+    {
+        image: 'Beany.jpg',
+        itemName: 'Beannie',
+        key: 'windy'
+    },
 
 
-  {
-      image: 'longsleeve.jpg',
-      itemName: 'Long sleeve t-shirt',
-      key: 'cold'
-  },
-  {
-      image: 'chino.jpg',
-      itemName: 'Chino',
-      key: 'cold'
-  },
-  {
-      image: 'coat.jpg',
-      itemName: 'Coat',
-      key: 'cold'
-  },
+    {
+        image: 'longsleeve.jpg',
+        itemName: 'Long sleeve t-shirt',
+        key: 'cold'
+    },
+    {
+        image: 'chino.jpg',
+        itemName: 'Chino',
+        key: 'cold'
+    },
+    {
+        image: 'coat.jpg',
+        itemName: 'Coat',
+        key: 'cold'
+    },
 
-  {
-      image: 'beany2.jpg',
-      itemName: 'Beannie',
-      key: 'cold'
-  },
-  {
-      image: 'sweater.jpg',
-      itemName: 'Jersey',
-      key:  'rainy'
-  },
-  {
-      image: 'jeans.jpg',
-      itemName: 'Jean',
-      key: 'rainy'
-  },
-  {
-      image: 'kwayJacket.jpg',
-      itemName: 'Jacket',
-      key:  'rainy'
-  },
+    {
+        image: 'beany2.jpg',
+        itemName: 'Beannie',
+        key: 'cold'
+    },
+    {
+        image: 'sweater.jpg',
+        itemName: 'Jersey',
+        key: 'rainy'
+    },
+    {
+        image: 'jeans.jpg',
+        itemName: 'Jean',
+        key: 'rainy'
+    },
+    {
+        image: 'kwayJacket.jpg',
+        itemName: 'Jacket',
+        key: 'rainy'
+    },
 
-  {
-      image: 'winterbennie.jpg',
-      itemName: 'Beannie',
-      key:  'rainy'
-  },
+    {
+        image: 'winterbennie.jpg',
+        itemName: 'Beannie',
+        key: 'rainy'
+    },
 ];
 
 var wardrobeTemplate = document.querySelector("#wardrobeTemplate").innerHTML;
+var wardrobeTemplateInstance = Handlebars.compile(wardrobeTemplate);
+var iziphumo = wardrobeTemplateInstance({
+    item: wardrobe
+});
 
 var whatToWear = function() {
-    var itemsForDay = [];
+    var itemsForTheDay = [];
 
 
     for (var i = 0; i < wardrobe.length; i++) {
 
-    if(inputWeather.value > 20 && inputWeather.value <= 30)
-    {
-      if(wardrobe[i].key === 'sunny')
-      {
-        itemsForDay.push(wardrobe[i]);
-      }
-    }
+        if (inputWeather.value > 20 && inputWeather.value <= 30) {
+            if (wardrobe[i].key === 'sunny') {
+                itemsForTheDay.push(wardrobe[i]);
+            }
+        }
 
-    if(inputWeather.value > 15 && inputWeather.value <= 20)
-    {
-      if(wardrobe[i].key === 'cold')
-      {
-        itemsForDay.push(wardrobe[i]);
-      }
-    }
+        else if (inputWeather.value > 15 && inputWeather.value <= 20) {
+            if (wardrobe[i].key === 'cold') {
+                itemsForTheDay.push(wardrobe[i]);
+            }
+        }
 
     }
 
-    return itemsForDay;
+    return itemsForTheDay;
 }
 
-whatToWear()
+var storeFunc = whatToWear();
 
-
-var wardrobeTemplateInstance = Handlebars.compile(wardrobeTemplate);
 var iziphumo = wardrobeTemplateInstance({
     item: whatToWear()
 });
 displayClothes.innerHTML += iziphumo;
 
-console.log(whatToWear());
+console.log(storeFunc);
 
 inputWeather.addEventListener('change', whatToWear)
